6dfea22822f9e1b46cb8f30436318c3924c2f228
bhawnarathor1204 <brathor@sebonetechnologies.com>
2022-04-21 11:48:28 +0530

    create some new feature

--- LibraryManagementProject/MockData/MockDo.cs
+++ LibraryManagementProject/MockData/MockDo.cs
@@ -12,16 +12,16 @@ namespace LibraryManagementProject.MockData
         public List<BookResponseDo> getMockData()
         {
             List<BookResponseDo> bookList = new List<BookResponseDo>();
-            //bookList.Add(new BookResponseDo(101,"The great gatsBy","henry",05/04/2022,10/04/2022,123456,"return","Fiction"));
-            //bookList.Add(new BookResponseDo(102, "Henry Miller", "andy", 10/04/2022, 15/04/2022, 87874838, "Not return", "NonFiction"));
-            //bookList.Add(new BookResponseDo(103, "Sarah water", "waltous", 05/04/2022, 10/04/2022, 964728, "return", "Historical"));
-            //bookList.Add(new BookResponseDo(104, "The vagetarian", "milan kudera", 05/04/2022, 10/04/2022, 7847984, "Non return", "Journal"));
-            //bookList.Add(new BookResponseDo(105, "All the light we can not see", "stifen king", 05/04/2022, 10/04/2022, 7463764, "return", "Fiction"));
-            //bookList.Add(new BookResponseDo(106, "Animal farm", "arthur golden", 05/04/2022, 10/04/2022, 344355, "Non return", "NonFiction"));
-            //bookList.Add(new BookResponseDo(107, "The bell jar", "ernest emigy", 05/04/2022, 10/04/2022, 746327, "return", "Historical"));
-            //bookList.Add(new BookResponseDo(108, "The Help", "leo tolstoy", 05/04/2022, 10/04/2022, 897859, "Non return", "Journal"));
-            //bookList.Add(new BookResponseDo(109, "Maus", "franz kafza", 05/04/2022,10/04/2022, 6356216, "return", "Fiction"));
-            //bookList.Add(new BookResponseDo(110, "War and Peas", "cormac", 05/04/2022, 10/04/2022, 6476318, "Non return", "NonFiction"));
+            bookList.Add(new BookResponseDo(101, "The great gatsBy", "henry", new DateTime(2022,03,10), new DateTime(2022,03,15), 123456, "return", "Fiction"));
+            bookList.Add(new BookResponseDo(102, "Henry Miller", "andy", new DateTime(2022,04,10 ), new DateTime(2022,05,15), 87874838, "Not return", "NonFiction"));
+            bookList.Add(new BookResponseDo(103, "Sarah water", "waltous", new DateTime(2022, 04, 15), new DateTime(2022, 04, 18), 964728, "return", "Historical"));
+            bookList.Add(new BookResponseDo(104, "The vagetarian", "milan kudera", new DateTime(2022, 05, 15), new DateTime(2022, 05, 15), 7847984, "Non return", "Journal"));
+            bookList.Add(new BookResponseDo(105, "All the light we can not see", "stifen king", new DateTime(2022, 04, 15), new DateTime(2022, 04, 18), 7463764, "return", "Fiction"));
+            bookList.Add(new BookResponseDo(106, "Animal farm", "arthur golden", new DateTime(2022, 04, 10), new DateTime(2022, 05, 15), 344355, "Non return", "NonFiction"));
+            bookList.Add(new BookResponseDo(107, "The bell jar", "ernest emigy", new DateTime(2022, 03, 10), new DateTime(2022, 03, 15), 746327, "return", "Historical"));
+            bookList.Add(new BookResponseDo(108, "The Help", "leo tolstoy", new DateTime(2022, 04, 10), new DateTime(2022, 05, 15), 897859, "Non return", "Journal"));
+            bookList.Add(new BookResponseDo(109, "Maus", "franz kafza", new DateTime(2022, 03, 10), new DateTime(2022, 03, 15), 6356216, "return", "Fiction"));
+            bookList.Add(new BookResponseDo(110, "War and Peas", "cormac", new DateTime(2022, 04, 10), new DateTime(2022, 05, 15), 6476318, "Non return", "NonFiction"));
 
             return bookList;
         }
