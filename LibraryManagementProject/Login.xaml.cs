20ed7cf813209186448f40d3f07d3699a1fe8851
bhawnarathor1204 <brathor@sebonetechnologies.com>
2022-04-21 15:21:37 +0530

    after error resolve

--- LibraryManagementProject/Login.xaml.cs
+++ LibraryManagementProject/Login.xaml.cs
@@ -38,14 +38,14 @@ namespace LibraryManagementProject
             this.Close();
 
         }
-        //private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        //{
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
 
-        //}
-        //private void Button_Click(object sender, RoutedEventArgs e)
-        //{
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
 
-        //}
+        }
 
         private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
         {
